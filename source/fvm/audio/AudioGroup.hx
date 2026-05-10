3ed3a7064c8c820866f1eb916ca7fb24270acfd3
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-10 11:10:47 -0500

    sound keys

--- source/fvm/audio/AudioGroup.hx
+++ source/fvm/audio/AudioGroup.hx
@@ -10,7 +10,49 @@ class AudioGroup extends FlxSoundGroup
 	public function update(elapsed:Float) {}
 
 	/**
-	 * Load a list of audio files
+	 * Think of it like a mental map of file names for the audio files
+	 */
+	public var soundKeys:Array<String> = [];
+
+	override function remove(sound:FlxSound):Bool
+	{
+		if (sounds.contains(sound))
+			soundKeys.remove(soundKeys[sounds.indexOf(sound)]);
+
+		return super.remove(sound);
+	}
+
+	/**
+	 * Returns the sound key of index `index` from `soundKeys` or a dummy string 
+	 * 
+	 * @param index 
+	 */
+	public function getSoundKey(index:Int)
+	{
+		return soundKeys[index] ?? 'Sound #$index';
+	}
+
+	/**
+	 * Load an audio file
+	 * 
+	 * **WILL NOT VERIFY THAT IT IS AN AUDIO FILE,
+	 * ONLY THAT THE FILE EXISTS.**
+	 * 
+	 * @param files Audio files list
+	 */
+	public function addSoundFile(file:String)
+	{
+		if (Assets.exists(file))
+		{
+			soundKeys.push(file);
+			add(new FlxSound().loadEmbedded(file));
+		}
+		else
+			trace('Audio File doesnt exist: $file');
+	}
+
+	/**
+	 * Load a list of audio files via `addSoundFile`
 	 * 
 	 * **WILL NOT VERIFY THAT IT IS AN AUDIO FILE,
 	 * ONLY THAT THE FILE EXISTS.**
@@ -20,12 +62,7 @@ class AudioGroup extends FlxSoundGroup
 	public function loadFiles(files:Array<String>)
 	{
 		for (file in files)
-		{
-			if (Assets.exists(file))
-				add(new FlxSound().loadEmbedded(file));
-			else
-				trace('Audio File doesnt exist: $file');
-		}
+			addSoundFile(file);
 	}
 
 	public function play(startTime:Float = 0, forceRestart:Bool = false)
@@ -55,11 +92,11 @@ class AudioGroup extends FlxSoundGroup
 
 			if (timeDifference < -range || timeDifference > range)
 			{
-				trace('Sound #${i + 1} : ${timeDifference}ms difference');
+				trace('${getSoundKey(i)} : ${timeDifference}ms difference');
 
 				sound.pause();
 				sound.time = sounds[0].time;
-				sound.play();
+				sound.resume();
 			}
 		}
 	}
