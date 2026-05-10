fb3f75725a731ba3a70019d8acae76ec9c6fbdf5
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-10 10:25:53 -0500

    contemplating hidding the doc.xml file...

--- docs/docs-windows.bat
+++ docs/docs-windows.bat
@@ -2,6 +2,6 @@
 cd /d "%~dp0.."
 echo Generating documentation...
 echo Platform is based on Windows
-haxelib run lime build windows --haxeflag="-xml docs/doc.xml" -D doc-gen -D DOCUMENTATION --no-output
+haxelib run lime build windows -D GENERATE_DOCS
 
 echo The XML file for the API documentation has been generated at docs/doc.xml.
