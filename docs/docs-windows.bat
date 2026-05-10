2c9e662243c2ef7a126fb77a982eb148568b68ba
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-10 10:27:16 -0500

    Autorunning html gen file

--- docs/docs-windows.bat
+++ docs/docs-windows.bat
@@ -5,3 +5,6 @@ echo Platform is based on Windows
 haxelib run lime build windows -D GENERATE_DOCS
 
 echo The XML file for the API documentation has been generated at docs/doc.xml.
+
+echo Autorunning html gen file
+docs/html.bat
