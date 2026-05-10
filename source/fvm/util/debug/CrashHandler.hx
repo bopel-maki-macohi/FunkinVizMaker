556f92642ead55127a692b094bbe4a29aa5f94a0
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-10 14:45:35 -0500

    start from scratch

--- source/fvm/util/debug/CrashHandler.hx
+++ source/fvm/util/debug/CrashHandler.hx
@@ -15,78 +15,13 @@ class CrashHandler
 {
 	public static function init()
 	{
-		if (FlxG.signals.postUpdate.has(errorKeybind)) return;
-
 		Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, onUncaughtError);
-
-		FlxG.signals.postUpdate.add(errorKeybind);
 
 		trace('Init crash handler');
 	}
 
-	public static var CRASH_KEY:FlxKey = F1;
-
-	static function errorKeybind()
-	{
-		if (FlxG.keys.anyJustPressed([CRASH_KEY])) throw 'Crash via ${CRASH_KEY.toString()}';
-	}
-
-	public static var CRASH_DIRECTORY:String =
-		#if debug
-		'../../../../dump/crash';
-		#else
-		'crash';
-		#end
-
 	static function onUncaughtError(event:UncaughtErrorEvent)
 	{
-		var path:String = '$CRASH_DIRECTORY/Crash Log ${DateUtil.generateCurrentFileTimestamp()}.log';
-		final spacing:String = '--------------------------------\n';
-
-		if (!FileSystem.exists(CRASH_DIRECTORY)) FileSystem.createDirectory(CRASH_DIRECTORY);
-
-		var UE:String = '';
-
-		try
-		{
-			UE = 'Uncaught Error: ${Std.string(event?.error) ?? 'Unknown'}';
-		}
-		catch (e)
-		{
-			UE = 'Uncaught Error: Unknown ($e)';
-		}
-
-		var errorMessage:String = '$UE\n\n';
-
-		errorMessage += 'Exception Stack:\n';
-		errorMessage += StackItemListParser.parse(CallStack.exceptionStack(true));
-
-		// errorMessage += '\nCall Stack:\n';
-		// errorMessage += StackItemListParser.parse(CallStack.callStack());
-
-		errorMessage += '\n$spacing\n';
-
-		errorMessage += 'Version: ${Application.current.meta.get('version')}\n\n';
-
-		if (FlxG?.state != null)
-		{
-			errorMessage += 'Current State: ${Type.getClassName(Type.getClass(FlxG?.state))?.replace('.', '/') + '.hx' ?? 'None (how tf)'}\n';
-			if (FlxG?.state?.subState != null)
-				errorMessage += ' - Current Substate: ${Type.getClassName(Type.getClass(FlxG?.state?.subState))?.replace('.', '/') + '.hx' ?? 'None'}\n';
-		}
-
-		errorMessage += '\n$spacing\n';
-
-		errorMessage += 'Crash log saved to "$path"\n';
-		errorMessage += 'Please report to the github: https://github.com/bopel-maki-macohi/Fu-kit/issues';
-
-		File.saveContent(path, errorMessage);
-		Application.current.window.alert(errorMessage, UE);
-
-		Sys.println(errorMessage);
-
-		Sys.sleep(2);
-
-		Sys.exit(0);
+		trace('Error');
 	}
 }
