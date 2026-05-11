1f24e8f7ecc5a1ff62ddf3f763e084422458ead0
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-11 15:38:39 -0500

    no it doesnt, FUCK.

--- source/fvm/graphics/VizPropGroup.hx
+++ source/fvm/graphics/VizPropGroup.hx
@@ -83,7 +83,7 @@ class VizPropGroup extends FlxSpriteGroup
 		}
 	}
 
-	public function getPropsOfID(propID:String):Array<IVizProp>
+	public function getPropsOfID(propID:String)
 	{
 		return members.filter(function(prop)
 		{
@@ -98,7 +98,7 @@ class VizPropGroup extends FlxSpriteGroup
 		return getPropsOfID(propID).length > 0;
 	}
 
-	public function getProp(propID:String):IVizProp
+	public function getProp(propID:String)
 	{
 		if (!propExists(propID)) return null;
 
