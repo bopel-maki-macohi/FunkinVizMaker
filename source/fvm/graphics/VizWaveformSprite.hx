db0fb689b2205c578b0f1019b7c177128d64c94f
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-11 16:51:09 -0500

    missing sc

--- source/fvm/graphics/VizWaveformSprite.hx
+++ source/fvm/graphics/VizWaveformSprite.hx
@@ -68,7 +68,7 @@ class VizWaveformSprite extends FlxWaveform implements IVizProp
 
 			if (waveformData?.bgColor != null) waveformBgColor = FlxColor.fromString(waveformData.bgColor);
 
-			waveformBarSize = waveformData?.barSize ?? 1
+			waveformBarSize = waveformData?.barSize ?? 1;
 			waveformBarPadding = waveformData?.barSizePadding ?? 0;
 
 			loaded = waveformBuffer != null;
